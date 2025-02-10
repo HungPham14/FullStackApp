41ab0477b18daf2c3368202090b7cd008a01213c
Hung Pham <96170868+HungPham14@users.noreply.github.com>
2025-02-10 17:05:21 +0700

    Install New Package and Modify index
    - react-native-community/geolocation package
    - test and error show user coordinates with above package

--- locate-me-app/app/index.tsx
+++ locate-me-app/app/index.tsx
@@ -1,25 +1,53 @@
 // Import packages
 import React, { useEffect, useState } from "react";
-import { Text, View, StyleSheet } from "react-native";
-// import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { Text, View, StyleSheet, Button, Alert } from "react-native";
 import MapView, {Marker} from "react-native-maps";
+import Geolocation from "@react-native-community/geolocation";
+import type { GeolocationOptions } from '@react-native-community/geolocation';
+// import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 // import { AppleMaps } from "expo-maps"; // this component can't be used in Expo Go.
 
-// Export default function Index
+// state to hold location
+// const [location, setLocation] = useState<string | null>(null);
+
+// log and set location with user location if successful
+// const getCurrentLocation = () => {
+//   Geolocation.getCurrentPosition(
+//     (position) => {
+//       console.log(position);
+//       // set position type to string and set state
+//       setLocation(JSON.stringify(position));
+//     },
+//     (error) => Alert.alert('GetCurrentPosition Error', JSON.stringify(error))
+//     ,
+//     { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+//   )
+// };
+
+// Preview User Location with Geolocation
+// export default function Index() {
+//   return (
+//     <View>
+//       <Text>Testing Current User Location</Text>
+//       <Text style={styles.baseText}>Position: </Text>{location || 'unknown'}
+//       <Button title="Test" onPress={getCurrentLocation} />
+//     </View>
+//   )
+// };
+
+// Preview Map with User Location
 export default function Index() {
   return (
     <View style={styles.container}>
-      {/* <Text style={styles.baseText}>Latitude: </Text>
-      <Text style={styles.baseText}>Longitude: </Text> */}
-      {/* <MapView showsUserLocation={true}></MapView> */}
       <MapView
         style={styles.map}
         showsUserLocation={true}
+        followsUserLocation={true}
+        showsScale={true}
+        userLocationCalloutEnabled={true}
       >
       </MapView>
-      {/* <MapView style={styles.map}></MapView> */}
     </View>
-    // <AppleMaps.View style={{ flex: 1 }} />
   );
 }
 
